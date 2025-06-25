6211258bf767c8ee42dc2007e15c322249884950
pablorgu <pablor623@gmail.com>
2025-06-25 22:34:58 +0200

    feat(CORS): Add CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: 'http://localhost:8081',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization'
+  });
+
   await app.listen(3000);
 }
 bootstrap();
