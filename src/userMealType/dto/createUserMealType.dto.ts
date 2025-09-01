7bd3f9414b3b6e0301b611595686bd05b1fc0cdc
pablorgu <pablor623@gmail.com>
2025-09-01 02:12:49 +0200

    refactor: remove userMealType module and related files

--- src/userMealType/dto/createUserMealType.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateUserMealTypeDto {
-  userId: number;
-  mealTypeId: number;
-  name: string;
-  order: number;
-}
-
--- src/userMealType/dto/queryUserMealType.dto.ts
+++
@@ -1,7 +0,0 @@
-export class QueryUserMealTypeDto {
-  userId?: number;
-  mealTypeId?: number;
-  name?: string;
-  order?: number;
-}
-
